6dfff46b15ee08d15d695a5015f63aeb932d298e
Nathan McNeill <nmcneill@gmail.com>
2016-06-29 16:49:58 -0500

    Static Site

--- js/app.js
+++ js/app.js
@@ -1,9 +1,7 @@
 $(function() {
 
   $("#balloon-figure").addClass("balloon-float");
-  var sectionIds = ["#way-up", "#sometimes-requires", "#digging-deep", "#biz-leaders", "#dedicated-to", "#hard-questions", "#hard-data", "#real-solutions", "#hard-question-header", "#chart-div", "#why-content", "#bridge-content", "#only-bridge"];
-  var prevSection;
-
+  var sectionIds = ["#way-up", "#sometimes-requires", "#digging-deep", "#biz-leaders", "#three-points", "#chart-div", "#bridge-content"];
   $(window).scroll(fadeInContent);
 
   $("#to-top-link").on("click", toTop);
@@ -11,16 +9,14 @@ $(function() {
   function fadeInContent() {
     var winTop = $(window).scrollTop();
     $.each(sectionIds, function(i, section) {
-      sectionPosition = $(section).offset().top - 600;
-      if (winTop > sectionPosition) {
-        console.log("Fade In " + section);
-        $(section).addClass("fade-in");
-        if (prevSection === undefined) {
-          prevSection = section;
-        } else if (prevSection !== section){
-          console.log("Fade Out " + prevSection);
-          $(prevSection).addClass("fade-out").removeClass("fade-in");
-          prevSection = section;
+      var sectionTop = $(section).offset().top;
+      var sectionBottom = sectionTop + $(section).outerHeight();
+      var prevSectionIndex = sectionIds.indexOf(section) - 1;
+
+      if (winTop > sectionTop - 500 && winTop < sectionTop - 450) {
+        $(section).addClass("showing");
+        if (prevSectionIndex !== -1) {
+          $(sectionIds[prevSectionIndex]).removeClass("showing");
         }
       }
     });
@@ -33,4 +29,5 @@ $(function() {
     });
     $("body").animate( { scrollTop: top }, 3000, "easeInOutQuint");
   }
+
 });
