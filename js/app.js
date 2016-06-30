5a2f1f6869a6463c12c4aa67bef3cd4bbce20e43
Nathan McNeill <nmcneill@gmail.com>
2016-06-30 08:09:16 -0500

    Completed Static Site

--- js/app.js
+++ js/app.js
@@ -1,26 +1,32 @@
 $(function() {
 
   $("#balloon-figure").addClass("balloon-float");
+
+  var invisible = [".tree-spacer"];
+  $.each(invisible, function(i, element) {
+    $(element).hide();
+  });
+
   var sectionIds = ["#way-up", "#sometimes-requires", "#digging-deep", "#biz-leaders", "#three-points", "#chart-div", "#bridge-content"];
   $(window).scroll(fadeInContent);
 
   $("#to-top-link").on("click", toTop);
 
-  function fadeInContent() {
-    var winTop = $(window).scrollTop();
-    $.each(sectionIds, function(i, section) {
-      var sectionTop = $(section).offset().top;
-      var sectionBottom = sectionTop + $(section).outerHeight();
-      var prevSectionIndex = sectionIds.indexOf(section) - 1;
-
-      if (winTop > sectionTop - 500 && winTop < sectionTop - 450) {
-        $(section).addClass("showing");
-        if (prevSectionIndex !== -1) {
-          $(sectionIds[prevSectionIndex]).removeClass("showing");
-        }
-      }
-    });
-  }
+  // function fadeInContent() {
+  //   var winTop = $(window).scrollTop();
+  //   $.each(sectionIds, function(i, section) {
+  //     var sectionTop = $(section).offset().top;
+  //     var sectionBottom = sectionTop + $(section).outerHeight();
+  //     var prevSectionIndex = sectionIds.indexOf(section) - 1;
+  //
+  //     if (winTop > sectionTop - 500 && winTop < sectionTop - 450) {
+  //       $(section).addClass("showing");
+  //       if (prevSectionIndex !== -1) {
+  //         $(sectionIds[prevSectionIndex]).removeClass("showing");
+  //       }
+  //     }
+  //   });
+  // }
 
   function toTop() {
     var top = $("#balloon-section").offset().top;
