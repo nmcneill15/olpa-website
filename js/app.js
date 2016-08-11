dc6908fc073f0d690508d04cd81dc80d485a8c11
Nathan McNeill <nmcneill@gmail.com>
2016-08-11 14:20:27 -0500

    changed real solutions section

--- js/app.js
+++ js/app.js
@@ -1,9 +1,11 @@
 $(function() {
 
   var menuShowing = false;
+  var bridgeBalloonIn = false;
 
   function load() {
-    setTimeout(showPage, 2000);
+    $(".load-clouds").fadeIn();
+    setTimeout(showPage, 1500);
   }
 
   load();
@@ -22,5 +24,21 @@ $(function() {
       menuShowing = false;
     }
   });
+
+
+  $(window).on("scroll", function() {
+    var winTop = $(window).scrollTop();
+    var trigger = $("#bridge-content").offset().top;
+    if (winTop > trigger && !bridgeBalloonIn) {
+      showBalloon();
+      bridgeBalloonIn = true;
+    }
+  });
+
+
+
+  function showBalloon() {
+    $(".real-solutions-section").animate( {"margin-top": 0}, 2000, "easeOutQuint");
+  }
 
 });
