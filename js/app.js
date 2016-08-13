52564da01a065d25c826bd139fe36356affc27a2
Nathan McNeill <nmcneill@gmail.com>
2016-08-13 14:30:55 -0500

    added animation to bridge

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 $(function() {
 
   var menuShowing = false;
+  var bridgeIn = false;
   var bridgeBalloonIn = false;
 
   $(window).on("resize", function() {
@@ -34,14 +35,20 @@ $(function() {
     var viewportWidth = $(window).width();
     var standard = 1024 / viewportWidth;
     var winTop = $(window).scrollTop() + (Math.floor(standard) * 200);
-    var trigger = $("#bridge-content").offset().top;
-    if (winTop > trigger && !bridgeBalloonIn) {
+    var balloonTrigger = $("#bridge-content").offset().top;
+    var bridgeTrigger = $(".hard-data-heading").offset().top - 500;
+    if (winTop > balloonTrigger && !bridgeBalloonIn) {
       showBalloon();
       bridgeBalloonIn = true;
+    } else if (winTop > bridgeTrigger && !bridgeIn) {
+      showBridge();
+      bridgeIn = true;
     }
   });
 
-
+  function showBridge() {
+    $("#only-bridge").animate( {opacity: 1, "margin-top": 0}, 2000, "easeOutQuint" );
+  }
 
   function showBalloon() {
     $(".real-solutions-section").animate( {"margin-top": 0}, 2000, "easeOutQuint");
